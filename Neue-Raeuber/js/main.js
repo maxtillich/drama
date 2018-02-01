a84faeb77993a9ab0aeae6f2ff3fcfe2cb86b922
Max Tillich <mtillich@gmail.com>
2018-02-01 16:15:39 +0100

    cover

--- Neue-Raeuber/js/main.js
+++ Neue-Raeuber/js/main.js
@@ -4,10 +4,19 @@ document.addEventListener("DOMContentLoaded", function() {
     smoothScrolling: false
   });
 
-  setTimeout(function () {
-    $('.name').addClass('visible');
-    console.log('yo');
-  }, 900);
+  // setTimeout(function () {
+  //   $('.name').addClass('visible');
+  //   console.log('yo');
+  // }, 1005);
+
+  window.onscroll = function() {myFunction()};
+
+  function myFunction() {
+    setTimeout(function () {
+      $('.name').addClass('visible');
+      console.log('yo');
+    }, 1);
+  }
 
   console.log('ok')
 });
